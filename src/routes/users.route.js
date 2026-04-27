55fe93705b3f698e552fec559dc917813a48f358
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-04-27 14:18:08 +0700

    update dokumentasi

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,6 +3,7 @@ const { addUser, getUserById } = require('../controllers/user.controller');
 
 const router = express.Router();
 
+// Route untuk /users (POST untuk menambah user baru)
 router.post('/', addUser);
 router.get('/:id', getUserById); 
 
